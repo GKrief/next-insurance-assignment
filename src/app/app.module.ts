cd5742744e285684ac38190cccc959c1affdd3df
Guy Krief <guy.krief6@gmail.com>
2020-11-18 12:22:47 +0200

    first commit with initial structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MoviesScreenComponent } from './movies/movies-screen/movies-screen.component';
+import { MovieDetailsComponent } from './movies/movie-details/movie-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesScreenComponent,
+    MovieDetailsComponent
   ],
   imports: [
     BrowserModule
