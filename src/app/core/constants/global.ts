dcc6d4b8d40f03dca459ace2151077ea5a10e3b5
Guy Krief <guy.krief6@gmail.com>
2020-11-18 14:21:45 +0200

    generating global.ts file to hold all keys of the response data

---
+++ src/app/core/constants/global.ts
@@ -0,0 +1,8 @@
+export const ID_HEADER = 'id';
+export const TITLE_HEADER = 'title';
+export const SYNOPSIS_HEADER = 'synopsis';
+export const RATING_HEADER = 'rating';
+export const RELEASED_HEADER = 'released';
+export const RUNTIME_HEADER = 'runtime';
+export const SMALL_IMG_HEADER = 'image';
+export const LARGE_IMG_HEADER = 'largeimage';
