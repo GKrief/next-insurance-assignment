75831d1cd32721a28a38387eea1cc2050e16a0cd
Guy Krief <guy.krief6@gmail.com>
2020-11-19 16:43:11 +0200

    implemented movie-details.component in order to show a specific movie details

--- src/app/movies/movie-details/movie-details.component.ts
+++ src/app/movies/movie-details/movie-details.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {Movie} from '../../core/models/movie';
+import {MovieService} from '../movie.service';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,8 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-details.component.css']
 })
 export class MovieDetailsComponent implements OnInit {
+  selectedMovie: Movie;
 
-  constructor() { }
+  constructor(private movieService: MovieService,
+              private activatedRoute: ActivatedRoute) {
+    this.selectedMovie = movieService.getMovieById(parseInt(this.activatedRoute.snapshot.paramMap.get('id'), 10));
+    console.log(this.selectedMovie);
+  }
 
   ngOnInit(): void {
   }
