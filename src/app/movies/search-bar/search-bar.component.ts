c3004a431c3391e8241be5bdc2f9972345df2a22
Guy Krief <guy.krief6@gmail.com>
2020-11-21 12:39:26 +0200

    implemented logic to provide two options for search, including emitting the values to parent component

--- src/app/movies/search-bar/search-bar.component.ts
+++ src/app/movies/search-bar/search-bar.component.ts
@@ -1,4 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {SearchOptions} from '../../core/enums/search-options.enum';
+import {Search} from '../../core/models/search';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,15 +8,35 @@ import {Component, EventEmitter, OnInit, Output} from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
-  searchValue = '';
-  @Output() searchValueEmitter = new EventEmitter<string>();
-  constructor() { }
+  readonly SEARCH_OPTIONS = Object.values(SearchOptions);
 
-  ngOnInit(): void {
+  @Output() searchEmitter = new EventEmitter<Search>();
+  searchCriteria: Search;
+  selectedOption: string;
+  searchValue = '';
+
+  constructor() {
   }
 
+  ngOnInit(): void {
+    this.searchCriteria = new Search(this.SEARCH_OPTIONS[0], '');
+  }
 
-  onChange(): void {
-    this.searchValueEmitter.emit(this.searchValue);
+  isSearchByRating(): boolean {
+    return this.searchCriteria.searchOption === SearchOptions.ByRating;
+  }
+
+  onSearchValueChange(): void {
+    this.emitSearchCriteria();
+  }
+
+  onSearchOptionChange(option: SearchOptions): void {
+    this.searchCriteria.searchOption = option;
+    this.searchCriteria.searchValue = '';
+    this.emitSearchCriteria();
+  }
+
+  private emitSearchCriteria(): void {
+    this.searchEmitter.emit(this.searchCriteria);
   }
 }
