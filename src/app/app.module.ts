b77ebeec2bdc8772564d6306c923dc1900484cdb
Guy Krief <guy.krief6@gmail.com>
2020-11-21 11:39:27 +0200

    only-numbers directive added to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { SanitizeHtmlPipe } from './core/pipes/sanitize-html.pipe';
 import { SearchBarComponent } from './movies/search-bar/search-bar.component';
 import {FormsModule} from '@angular/forms';
 import { FilterBySearchPipe } from './core/pipes/filter-by-search.pipe';
+import { OnlyNumbersDirective } from './core/directives/only-numbers.directive';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { FilterBySearchPipe } from './core/pipes/filter-by-search.pipe';
     MovieLengthPipe,
     SanitizeHtmlPipe,
     SearchBarComponent,
-    FilterBySearchPipe
+    FilterBySearchPipe,
+    OnlyNumbersDirective
   ],
   imports: [
     BrowserModule,
