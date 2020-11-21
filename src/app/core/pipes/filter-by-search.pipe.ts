129c3b10ebf7afba946efe1f076030f8f3e70a1c
Guy Krief <guy.krief6@gmail.com>
2020-11-21 12:34:55 +0200

    implemented the filter pipe to handle both options of search

--- src/app/core/pipes/filter-by-search.pipe.ts
+++ src/app/core/pipes/filter-by-search.pipe.ts
@@ -1,17 +1,25 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 import {Movie} from '../models/movie';
+import {SearchOptions} from '../enums/search-options.enum';
 
 @Pipe({
   name: 'filterBySearch'
 })
 export class FilterBySearchPipe implements PipeTransform {
 
-  transform(movies: Movie[], searchInput: string): Movie[] {
-    if (!movies) {
-      return movies;
+  transform(movies: Movie[], searchOption: SearchOptions, searchValue: string): Movie[] {
+    if (searchOption === SearchOptions.ByTitle) {
+      if (!movies) {
+        return movies;
+      }
+
+      return movies.filter(movie => movie.title.toLowerCase().includes(searchValue.toLowerCase()));
+    } else { // by rating
+      if (!movies || searchValue === '') {
+        return movies;
+      }
+
+      return movies.filter(movie => movie.rating >= parseFloat(searchValue));
     }
-
-    return movies.filter(movie => movie.title.toLowerCase().includes(searchInput.toLowerCase()));
   }
-
 }
