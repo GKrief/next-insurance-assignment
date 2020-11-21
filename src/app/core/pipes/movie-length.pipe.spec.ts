4afc102e5174d6e231c34bd15b7a56476813c7ef
Guy Krief <guy.krief6@gmail.com>
2020-11-21 22:09:44 +0200

    added a simple test to movie-length pipe

--- src/app/core/pipes/movie-length.pipe.spec.ts
+++ src/app/core/pipes/movie-length.pipe.spec.ts
@@ -1,8 +1,13 @@
 import { MovieLengthPipe } from './movie-length.pipe';
 
 describe('MovieLengthPipe', () => {
+  const pipe = new MovieLengthPipe();
+
   it('create an instance', () => {
-    const pipe = new MovieLengthPipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('transforms "2h2m" to "2 hours, 2 minutes"', () => {
+    expect(pipe.transform('2h2m')).toBe('2 hours, 2 minutes');
   });
 });
